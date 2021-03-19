a133ec4aa178a379ce020276e84e88bde640329a
Olga <25307442+comm0nsense@users.noreply.github.com>
2021-03-19 22:39:09 +0300

    refactor: align scale value & size of image to  upload

--- js/upload-image.js
+++ js/upload-image.js
@@ -14,35 +14,35 @@ const imageScale = imageUpload.querySelector('.scale');
 const scaleControlValue = imageScale.querySelector('.scale__control--value');
 
 // let currentScale = parseInt(scaleControlValue.value);
+let currentScale;
 
 const onImageRescale = (evt) => {
-  let currentScale = parseInt(scaleControlValue.value);
+  currentScale = parseInt(scaleControlValue.value);
 
   if (evt.target.matches('.scale__control--smaller')) {
     currentScale = currentScale - ImageScale.RESCALE_STEP;
     if (currentScale >= ImageScale.MIN) {
-      scaleControlValue.value = `${currentScale}`;
+      scaleControlValue.value = `${currentScale}%`;
       imageUploadPreview.style.transform = `scale(${(currentScale) / ImageScale.FULL_SIZE})`;
     }
   } else {
-    scaleControlValue.value = ImageScale.MIN;
+    scaleControlValue.value = `${ImageScale.MIN}%`;
     imageUploadPreview.style.transform = `scale(${(ImageScale.MIN) / ImageScale.FULL_SIZE})`;
   }
 
   if (evt.target.matches('.scale__control--bigger')) {
     currentScale = currentScale + ImageScale.RESCALE_STEP;
     if (currentScale <= ImageScale.MAX) {
-      scaleControlValue.value = `${currentScale}`;
+      scaleControlValue.value = `${currentScale}%`;
       imageUploadPreview.style.transform = `scale(${(currentScale) / ImageScale.FULL_SIZE})`;
     } else {
-      scaleControlValue.value = ImageScale.MAX;
+      scaleControlValue.value = `${ImageScale.MAX}%`;
       imageUploadPreview.style.transform = `scale(${(ImageScale.MAX) / ImageScale.FULL_SIZE})`;
     }
   }
 };
 
-imageScale.addEventListener('click', onImageRescale);
-
+// imageScale.addEventListener('click', onImageRescale);
 
 
 const onUploadEscKeydown = (evt) => {
@@ -56,15 +56,19 @@ const onPictureUpload = () => {
   imageUpload.classList.remove('hidden');
   document.querySelector('body').classList.add('modal-open');
 
+  currentScale = parseInt(scaleControlValue.value);
+  imageUploadPreview.style.transform = `scale(${(currentScale) / ImageScale.FULL_SIZE})`;
+
   document.addEventListener('keydown', onUploadEscKeydown);
   imageUploadCancel.addEventListener('click', onCancelUpload);
+  imageScale.addEventListener('click', onImageRescale);
+
 };
 
 const onCancelUpload = () => {
   imageUpload.classList.add('hidden');
   document.querySelector('body').classList.remove('modal-open');
   imageUploadForm.reset();
-  // newImage.reset();
 
   document.removeEventListener('keydown', onUploadEscKeydown);
   imageUploadCancel.removeEventListener('click', onCancelUpload);
