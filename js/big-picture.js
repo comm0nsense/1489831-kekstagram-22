18f1517f11fc58b84760f8d59aba3d4771d50f18
Olga <25307442+comm0nsense@users.noreply.github.com>
2021-03-25 18:34:07 +0300

    refactor: complete comments loading feature

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,12 +1,4 @@
-// import { generateRandomPhotos } from './mock.js';
-// import { renderPreviews } from './render-previews.js';
 import { isEscEvent } from './util.js';
-
-//
-// const photos = generateRandomPhotos(25);
-// console.log(photos);
-
-// renderPreviews(photos);
 
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureImg = bigPicture.querySelector('.big-picture__img img');
@@ -16,7 +8,6 @@ const bigPictureCommentsCount = bigPicture.querySelector('.comments-count');
 const bigPictureComments = bigPicture.querySelector('.social__comments');
 const commentsLoader = bigPicture.querySelector('.comments-loader');
 const numberOfCommentsLoaded = bigPicture.querySelector('.comments-loaded');
-
 
 const picturesWrapper = document.querySelector('.pictures');
 const modalCloseButton = document.querySelector('.big-picture__cancel');
@@ -43,30 +34,31 @@ const createComments = (comments, number) => {
 };
 
 
-const onModalEscPress = (evt) => {
-  if (isEscEvent(evt)) {
-    evt.preventDefault();
-    bigPicture.classList.add('hidden');
-  }
-
-  // picturesWrapper.addEventListener('click', onPictureClick);
-  document.removeEventListener('keydown', onModalEscPress);
-};
-
-const closeModal = () => {
-  bigPicture.classList.add('hidden');
-  // document.querySelector('body').classList.remove('modal-open');
-  // bigPicture.querySelector('.social__comment-count').classList.remove('hidden');
-  // bigPicture.querySelector('.social__comments-loader').classList.remove('hidden');
-
-  document.removeEventListener('keydown', onModalEscPress);
-  modalCloseButton.removeEventListener('click', closeModal);
-  // picturesWrapper.addEventListener('click', onPictureClick);
-
-};
-
-
 const openModal = (photo) => {
+
+  const onModalEscPress = (evt) => {
+    if (isEscEvent(evt)) {
+      evt.preventDefault();
+      bigPicture.classList.add('hidden');
+    }
+
+    // picturesWrapper.addEventListener('click', onPictureClick);
+    document.removeEventListener('keydown', onModalEscPress);
+    commentsLoader.removeEventListener('click', onMoreCommentsClick);
+  };
+
+  const closeModal = () => {
+    bigPicture.classList.add('hidden');
+    // document.querySelector('body').classList.remove('modal-open');
+    // bigPicture.querySelector('.social__comment-count').classList.remove('hidden');
+    // bigPicture.querySelector('.social__comments-loader').classList.remove('hidden');
+    document.removeEventListener('keydown', onModalEscPress);
+    modalCloseButton.removeEventListener('click', closeModal);
+    commentsLoader.removeEventListener('click', onMoreCommentsClick);
+    // picturesWrapper.addEventListener('click', onPictureClick);
+  };
+
+
   bigPicture.classList.remove('hidden');
   // document.querySelector('body').classList.add('modal-open');
   // bigPicture.querySelector('.social__comment-count').classList.add('hidden');
@@ -75,29 +67,37 @@ const openModal = (photo) => {
   bigPictureLikes.textContent = photo.likes;
   bigPictureDescription.textContent = photo.description;
   bigPictureCommentsCount.textContent = photo.comments.length;
+
   if (photo.comments.length < 5) {
     numberOfCommentsLoaded.textContent = photo.comments.length;
+    commentsLoader.classList.add('hidden');
   } else {
     numberOfCommentsLoaded.textContent = '5';
+    commentsLoader.classList.remove('hidden');
   }
 
   createComments(photo.comments, 5);
 
   const onMoreCommentsClick = () => {
     const commentsTotal = photo.comments.length;
-    // console.log(p÷hoto.comments.length);
-    const commentsLoaded = bigPicture.querySelectorAll('.social__comment').length;
+    const commentsLoaded = bigPictureComments.children.length;
     const commentsBalance = commentsTotal - commentsLoaded;
     // console.log(`Total: ${commentsTotal}, loaded: ${commentsLoaded}, balance: ${commentsBalance}`);
-    if (commentsBalance >=5) {
+
+    if (commentsBalance > 5) {
       createComments(photo.comments, commentsLoaded + 5);
       numberOfCommentsLoaded.textContent = `${commentsLoaded +5}`;
+    } else if (commentsBalance === 5) {
+      createComments(photo.comments, commentsLoaded + 5);
+      numberOfCommentsLoaded.textContent = `${commentsLoaded +5}`;
+      commentsLoader.classList.add('hidden');
     } else {
       createComments(photo.comments, commentsTotal);
       commentsLoader.classList.add('hidden');
       numberOfCommentsLoaded.textContent = `${commentsTotal}`;
     }
-  }
+
+  };
 
 
   document.addEventListener('keydown', onModalEscPress);
